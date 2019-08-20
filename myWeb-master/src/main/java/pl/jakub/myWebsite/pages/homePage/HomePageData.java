97f542322404e5ebdf8e2867474dc056c614abdc
jakubs21 <92jakub.sadowski@gmail.com>
2019-08-20 18:43:32 +0200

    delete all

--- myWeb-master/src/main/java/pl/jakub/myWebsite/pages/homePage/HomePageData.java
+++
@@ -1,6 +0,0 @@
-package pl.jakub.myWebsite.pages.homePage;
-
-public class HomePageData {
-    public final static String WEBSITE_URL = "http://sadowski.thecompletewebhosting.com/index.php";
-    public static final String ASSERTION_ID = "instruction";
-}
